7bae263b8ef07c96b4b922ad05cc4c8cc621ea73
Iain Stewart <iain.stewart08@gmail.com>
2016-06-07 15:53:55 +0100

    finished functions

--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -75,7 +75,8 @@ class Functions_Practice < MiniTest::Test
 
   #Given the radius of a sphere calculate the volume
   def test_volume_of_sphere()
-
+    sphere_result = volume_of_sphere(3)
+    assert_equal(113.1, sphere_result)
   end
 
 
