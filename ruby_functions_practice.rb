a58b20bbb4da1bb1241dd5ee2433bcc88c27e6c6
Iain Stewart <iain.stewart08@gmail.com>
2016-06-07 15:50:47 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,101 @@
+def return_10()
+  return 10
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(num1, num2)
+  return num1 * num2
+end
+
+def divide(num1, num2)
+  return num1 / num2
+end
+
+def length_of_string(test_string)
+  return test_string.length
+end
+
+def join_string(str1, str2)
+  return "#{str1}#{str2}"
+end
+
+def add_string_as_number(str1, str2)
+  return str1.to_i + str2.to_i
+end
+
+def number_to_full_month_name(month_index)
+  case(month_index)
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  else
+    return "That's not a month"
+  end
+end
+
+def number_to_short_month_name(month_index)
+  case(month_index)
+  when 1
+    return "Jan"
+  when 2
+    return "Feb"
+  when 3
+    return "Mar"
+  when 4
+    return "Apr"
+  when 5
+    return "May"
+  when 6
+    return "Jun"
+  when 7
+    return "Jul"
+  when 8
+    return "Aug"
+  when 9
+    return "Sep"
+  when 10
+    return "Oct"
+  when 11
+    return "Nov"
+  when 12
+    return "Dec"
+  else
+    return "That's not a month"
+  end
+end
+
+def volume_of_cube(l, b, h)
+  return l * b * h
+end
+
+def volume_of_sphere(r)
+  return (4 / 3) * Math.pi * r ** 3
+end
